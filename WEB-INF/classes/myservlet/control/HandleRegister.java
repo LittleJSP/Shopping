fded49d996924094caf0382ce89b223d03bc3022
hexiangyan <45188772+hexiangyan@users.noreply.github.com>
2018-11-27 23:53:26 +0800

    香香更正
    校对全文

--- WEB-INF/classes/myservlet/control/HandleRegister.java
+++ WEB-INF/classes/myservlet/control/HandleRegister.java
@@ -1,94 +1,86 @@
-HandleRegister. java
-package myservlet control
-import mybean. data.*
-import Java.Sq.兴
-import java.io.兴;
-import javax. servlet.*
-importjavax.servlethttp.*i
-public class Handleregister extends Http servlet
-public void init( ServletConfig config)throws ServletException
-
-
-
-
-
-uper init(co
-try Class for Name("com mysql jdbc Driver");
-catch(Exception e)1)
-public String handleString( String s)
-I tryi byte bb[ ]=s getBytes("iso-8859-1")
-(bb)
-catch( Exception ee)I
-publicvoiddopost(httpservletrequEstrequestHttpservletreSponse
-response)throws ServletException, IOException t
-String uri="jdbc: mysql: //127.0.0. 1/shop? "+"user root&password =
-characterEncoding=gb2312"i
-Connection con
-PreparedStatement sql
-Register userBean= new Register()
-/创建的 Javabean模型
-request setAttribute("userBean", user Bean)i
-String logname= request getParameter("logname").trim()
-String password request getParameter("password"). trim()
-String again password= request getParameter("again password").trim()i
-String phone= request getParameter("phone"). trim(;
-String address request getParameter("address"). trim();
-String realname request getParameter("realname"). trimo
-if(logname = null)
-ifpassword ==null)
-if(! password equals(again password))I
-userBean. setBackNews("两次密码不同,注册失败,");
-RequestDispatcher dispatcher
-request getRequestDispatcher("inputRegisterMess jsp")
-dispatcher. forward(request, response)
-//转发
-10
-boolean isLD= true;
-for(int i=0; i<l
-length(): 1++)1
-char c= logname charAt(i)i
-if(!(c<=zsc>='a")‖(c<="z'sc>="A')‖(c<="9sc>=0)
-用户
-isLD= fal
-的用户名
-boolean boo= logname length()> 0&&password length()>0&&isLD
-该模
-String backNews =
-息到控制
-try con=DriverManager get Connection(uri)
-录的信息
-String insertCondition="INSERT INTO user VALUES(?,?,?,?,?)";
-sql=con. prepareStatement( insertCondition
-更新视图
-if(boo)
-I sql setString( 1, handleString(logname));
-String(2, handle
-ring( password))
-视图
-
-
-sql setString(3, handleString( phone))
-sql: setString(4, handleString( address))
-sql setString(5, handleString(realname))
-int m= sql executeUpdate(
-f(m!=0){
-backNews="注册成功";
-userBean setBack News(back News)i
-userBean.setLogname(logname);
-userBean. setPhone( handleString(phone))
-userBean. setAddress(handleString(address))i
-userBean. setRealname( handleString(realname))i
-else I
-backNews="信息填写不完整或名字中有非法字符";
-user Bean. setBackNews(back News)
-close()
-catch( SQLException exp)t
-backNews="该会员名已被使用,请您更换名字"+exp;
-userBean. setBackNews(backNews )i
-RequestDispatcher dispatcher
-request getRequestDispatcher("inputRegisterMess jsp")
-dispatcher. forward(request, response
-//转发
-public void doget(hTtpservletrequEst request HttpservletreSponse
-response) throws ServletException, IOException I
-doPost(request, response)
+package myservlet.control;
+import mybean.data.*;
+import java.sql.*;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+public class HandleRegister extends HttpServlet{
+	public void init(ServletConfig config)throws ServletException{
+		super.init(config);
+		try{Class.forName("com.mysql.jdbc.Driver");
+		}
+		catch(Exception e){}
+	}
+	public String handleString(String s)
+	{   try{byte bb[ ]=s.getBytes("iso-8859-1");
+	        s=new String(bb);
+	    }
+		catch(Exception ee){}
+		return s;
+	}
+	public void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+		String uri="jdbc:mysql://127.0.0.1/shop?"+"user=root&password=&characterEncoding=gb2312";
+		Connection con;
+		PreparedStatement sql;
+		Register userBean=new Register();
+		request.setAttribute("userBean",userBean);
+		String logname=request.getParameter("logname").trim();
+		String password =request.getParameter("password").trim();
+		String again_password=request.getParameter("again_password").trim();
+		String phone=request.getParameter("phone").trim();
+    String address=request.getParameter("address").trim();
+		String realname=request.getParameter("realname").trim();
+		if(logname==null)
+		logname="";
+		if(password==null)
+        password="";
+		if(!password.equals(again_password)){
+			userBean.setBackNews("两次密码不同，注册失败,");
+			RequestDispatcher dispatcher=request.getRequestDispatcher("inputRegisterMess.jsp");
+			dispatcher.forward(request,response);
+			return;
+		}
+		boolean isLD=true;
+		for(int i=0;i<logname.length();i++){
+			char c=logname.charAt(i);
+			if(!((c<='z'&&c>='a')||(c<='Z'&&c>='A')||(c<='9'&&c>='0')))
+			isLD=false;
+		}
+		boolean boo=logname.length()>0&&password.length()>0&&isLD;
+		String backNews="";
+		try{con=DriverManager.getConnection(uri);
+		String insertCondition="INSERT INTO user VALUES(?,?,?,?,?)";
+		sql=con.prepareStatement(insertCondition);
+		if(boo)
+		{sql.setString(1,handleString(logname));
+		 sql.setString(2,handleString(password));
+     sql.setString(3,handleString(phone));		
+     sql.setString(4,handleString(address));
+     sql.setString(5,handleString(realname));
+		int m=sql.executeUpdate();
+		if (m!=0){
+			backNews="注册成功";
+			userBean.setBackNews(backNews);
+			userBean.setLogname(logname);
+			userBean.setPhone(handleString(phone));
+			userBean.setAddress(handleString(address));
+			userBean.setRealname(handleString(realname));
+		  }
+		}
+		else{
+			backNews="信息填写不完整或名字中有非法字符";
+			userBean.setBackNews(backNews);
+		}
+		con.close();
+		}
+		catch(SQLException exp){
+			backNews="该会员名已被使用，请您更换名字" + exp;
+			userBean.setBackNews(backNews);
+		}
+		RequestDispatcher dispatcher=request.getRequestDispatcher("inputRegisterMess.jsp");
+		dispatcher.forward(request,response);
+	}
+	public void doGet(HttpServletRequest request,HttpServletResponse response)throws ServletException,IOException{
+		doPost(request,response);
+	}
+}
