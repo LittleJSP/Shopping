e36ca01b453eb154d186774db9b0a507e744947f
hexiangyan <45188772+hexiangyan@users.noreply.github.com>
2018-11-28 18:12:08 +0800

    香香更改1
    更正校对了

--- WEB-INF/classes/myservlet/control/HandleLogin.java
+++ WEB-INF/classes/myservlet/control/HandleLogin.java
@@ -1,119 +1,106 @@
-package myservlet. control;
-import mybean. data. * i
-import java.sq1.兴
-import Java.i0.头i
-port javax. servlet.*
-importJavax.servlethttp.*i
-mport java util
-public class Handlelogin extends Httpservlet
-public void init( ServletConfig config) throws ServletExceptiont
-super init(config)
-tryi
-Class for Name(" com mysql jdbc Driver")
-catch(Exception e)(
-public String handleString( String s)(
-try byte bb[ ]=s getBytes("iso-8859-1)
-s= new String(bb)
-catch(Exception ee)t)
-return si
-public void dopost(hTtpserv
-letRequest reques
-st,httpServletresponSe
-response) throws ServletException, IOExceptiont
-
-
-
-
-
-Connection coni
-Statement sqli
-").trim()
-ring log
-quest. getP
-Issword= request getParameter("password").trim()i
-handlestring( log
+package myservlet.control;
+import mybean.data.*;
+import java.sql.*;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.*;
+public class HandleLogin extends HttpServlet{
+public void init(ServletConfig config) throws ServletException{
+super.init(config);
+try{
+Class.forName("com.mysql.jdbc.Driver");
+}
+catch(Exception e){}
+}
+public String handleString(String s){
+try{ byte bb[ ]=s.getBytes("iso-8859-1");
+s= new String(bb);
+}
+catch(Exception ee){}
+return s;
+}
+public void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+Connection con;
+Statement sql;
+String logname=request.getParameter("logname").trim(),
+password=request.getParameter("password").trim();
+logname=handleString(logname);
 password=handleString(password);
-String uri="jdbc: mysql: //127.0.0. 1/shop? +"user=rootapassword
-&character Encoding= gb2312";
-boolean boo =(logname length()>0)&&(password length()>0);
-String condition ="select from user where logname s ."+ logname t
-d+
-1=con. createStatement()
-ResultSet rs= sql
-dition)
-boolean m=rs. next
-(m== true)[
+String uri="jdbc:mysql://127.0.0.1/shop?"+ "user=root&password=&characterEncoding=gb2312";
+boolean boo=(logname.length()>0)&&(password.length()>0);
+try{
+con=DriverManager.getConnection(uri);
+String condition="select * from user where logname='"+logname+"'and password='"+password+"'";
+sql=con.createStatement();
+if(boo){
+ResultSet rs=sql.executeQuery(condition);
+boolean m=rs.next();
+if(m==true){
 //调用登录成功的方法:
-success(request, response, logname, password)i
-RequestDispatcher dispatcher
-request getRequestDispatcher("login. jsp"); //454
-dispatcher. forward(request, response)
+success(request,response,logname, password);
+RequestDispatcher dispatcher=request.getRequestDispatcher("login.jsp"); //转发
+dispatcher.forward(request,response);
+}
+else{
 String backNews="您输人的用户名不存在,或密码不匹配";
-//调用登录失败的方法
-i1(
-back News)
+//调用登录失败的方法:
+fai1(request,response,logname,backNews);
+}
+}
+else{
 String backNews="请输入用户名和密码";
-quest
-ogame, back News)
-close()
-catch(SQLException exp)(
-String backNews
-n"+ exp
-fail(
-public void doget(HttpservletreQuest request HttpservletreSponse
-response)throws
-ServletExcepti
-doPost(
-on, IOException
-d
-(Ht
-1
-String
-request
-str
-Login loginBean= null
-Httpsession sessions request getsession(true);
-tr
-1
-(Login
-oginBe
-
-
-
-
-
-
-loginBean= new Login
-session. setAttribute("loginBean ", loginBean)i
-//创建新的数据模型
-oginBe
-gin)
-tribute(
-r工r
-login
-get
-if(name, equals(logname))(
-1 oginBean. setBackNews(1 ogame+"已经登录了");
-glnb
-else i
-loginBean. setBackNews( logname+"登录成功");
-//数据模型存储新的登录用户
-login
-gname( logname)
-catch( Exception ee)
-session. setAttribute("loginBean", loginBean);
-loginBean. setBackNews(1 ogame+"登录成功");
-oginBean setL
-(logname
-publicvoidfail(httpservlEtrequestrequestHttpservleTresponseresponse
-String logname, String backNews)t
-response. setContentType("text/html; charset= GB2312")
-try i
-PrintWriter out= response. getWriter()i
-out println("< html > body >)i
-out. print1n("<h2>"+1 ogame+"登录反馈结果<br>"+ backNews+"</h2>");
-out. print1n("返回登录页面或主页<br>");
-out. print1n("< a href= login.jsp>登录页面</a>");
-out println("<br>< a href index jsp >ED</a>)
-out println("</body ></htm1>)i
-catch( IOException exp)f)
+fail(request,response,logname,backNews);
+}
+con.close();
+}
+catch(SQLException exp){
+String backNews=""+exp;
+fail(request,response,logname,backNews);
+}
+}
+public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+doPost(request,response);
+}
+public void success (HttpServletRequest request,HttpServletResponse response,String logname,String password){
+Login loginBean=null;
+HttpSession session=request.getSession(true);
+try{
+loginBean=(Login)session.getAttribute("loginBean");
+if(loginBean==null){
+loginBean=new Login();
+session.setAttribute("loginBean",loginBean);
+loginBean=(Login)session.getAttribute("loginBean");
+}
+String name=loginBean.getLogname();
+if(name.equals(logname)){
+loginBean.setBackNews(logname+"已经登陆了");
+loginBean.setLogname(logname);
+}
+else{
+loginBean.setBackNews(logname+"登陆成功");
+loginBean.setLogname(logname);
+}
+}
+catch(Exception ee){
+loginBean=new Login();
+session.setAttribute("loginBean",loginBean);
+loginBean.setBackNews(logname+"登陆成功");
+loginBean.setLogname(logname);
+}
+}
+public void fail(HttpServletRequest request,HttpServletResponse response,
+String logname,String backNews){
+response.setContentType("text/html;charset= GB2312");
+try{
+PrintWriter out = response.getWriter();
+out.println("<html><body>");
+out.print1n("<h2>"+logname+"登录反馈结果<br>"+backNews+"</h2>");
+out.print1n("返回登录页面或主页<br>");
+out.print1n("<a href = login.jsp>登录页面</a>");
+out.println("<br><a href = index.jsp>主页</a>");
+out.println("</body></htm1>");
+}
+catch(IOException exp){}
+}
+}
