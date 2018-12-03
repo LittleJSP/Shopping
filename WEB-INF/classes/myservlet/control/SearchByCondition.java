42dbb09d11171ae64d584eeab62b21fd2a813a97
AndrewVanilla <240924154@qq.com>
2018-12-03 09:24:21 +0800

    查询商品功能编辑完毕（差编译）

--- WEB-INF/classes/myservlet/control/SearchByCondition.java
+++ WEB-INF/classes/myservlet/control/SearchByCondition.java
@@ -1,97 +1,96 @@
-package myservlet control
-import mybean. data. DataByeage
-import com. sun rowset. * i
-import java. sql.*i
-import Java.10.米
-import javax. servlet.*
-importjavax.servlethttp.*i
-public class Searchbycondition extends Httpservleti
-CachedRowSetImpl rowSet= nulli
-
-
-
-
-public void init( ServletConfig config) throws ServletExceptionf
-super init(config);
-try[ class forName(com. mysql jdbc Driver");
-catch(Exception e)
-public void dopost(HttpservletrequeSt request HttpservletreSponse
-response) throws ServletException, IOException
-request setCharacterEncoding("gb2312")
-String searchLess= request getParameter("searchLess")
-String radioMess= request getParameter("radio")
-if(searchLess ==null searchLess length()==0)
-fail( request, response,"没有查询信息,无法查询");
-return
-String condition=i
-if(radioMess equals("cosmetic number"))i
-condition
-"SELECt FROM cosmeticForm where cosmetic number ='" searchLess +"i
-else if(radioMess equals("cosmetic_name"))t
-condition ="SETECT FROM cosmeticForm where cosmetic_name LTKE'8+ searchLess +8,
-else if(radioMess equals("cosmetic_price"))I
-double max=0, min=0;
-String regex="[^0123456789.]";
-String[ priceMess searchMess split(regex)
-if(priceLess length==1)
-max =min Double parseDouble(priceLess[ o])i
-e⊥se
-f(priceLess length == 2)
-min= Double. parseDouble(priceLess[O])
-max= Double. parseDoublepriceLess[1]);
-if(max min)
-double t = max
-in t
-从
-到
-else i
-fail( request, response,"输入的价格格式有错误");
-return;
-condition =SELECT FROM cosmeticForm where+
-"cosmetic_price <= max+ AND cosmetic_price>=+min i
-Httpsession session request getsession(True);
-单行资
-Connection con null
-DataBy Page dataBean= null;
-dataBean=(DataByPage)session. getAttribute("dataBean"
-
-
-
-
-f( dataBean ==nu
-dataBean new DataBy Page()
-//创建 Javabean对象
-ession setAttribute("dataBean" data
-catch( Exception exp
-dataBean new DataByPage )i
-session. setAttribute("dataBean", dataBean
-String
-1://127.0.0.1/sh
-Muser root&password =&characterEncoding=gb2312"
-con= DriverManager get Connection(uri);
-Statement
-sql=con. createStatement( ResultSet. TYPE SCROLL SENSITIVE,
-Resultset CONCUR READ ONLY)
-ResultSet rs= sql executeQuery( condition)
-rowSet= new CachedRowSetImp1();
-//创建行集对象
-Set populate(rs)i
-dataBean. setRowSet (rowSet);
-//行集数据存储在 dataBean中
-con. close();
-/关闭连接
-catch( SQLException exp)I
-response. sendRedirect("byPage Show jsp")
-//重定向到 by Pageshow.jsp
-public void doget(hTtpservletreqUest request HttpservletresPonse
-response) throws ServletException, IOExceptionI
-doPost(request, response)
-publicvoidfail(httpservlEtrequestrequestHttpservletResponseresponseStringbacknews)
-response.setContentType("text/html; charset= GB2312")
-PrintWriter out= response. getWriter();
-out println("<html > body>")
-outprintln("<h2>+ back News +</h2>")
-out. println("返回:")
-out. println("< a href= searchCosmetic.jsp>查询化妆品</a>");
-out println("</body ></htm1>")
-catch(IOException exp)(unn-0At
+package myservlet.control;
+import mybean.data.DataByPage;
+import com.sun.rowset.*;
+import java.sql.*;
+import Java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+public class SearchByCondition extends HttpServlet{
+  CachedRowSetImpl rowSet= null;
+  public void init(ServletConfig config) throws ServletException{
+    super.init(config);
+    try{
+      Class.forName("com.mysql.jdbc.Driver");
+    }
+    catch(Exception e){}
+  }
+  public void doPost(HttpServletRequeSt request,HttpServletResponse response) throws ServletException, IOException{
+    request.setCharacterEncoding("gb2312");
+    String searchMess= request.getParameter("searchMess");
+    String radioMess= request.getParameter("radio");
+    if(searchMess== null||searchMess.length()==0){
+      fail(request,response,"没有查询信息,无法查询");
+      return;
+    }
+    String condition="";
+    if(radioMess.equals("cosmetic_number")){
+      condition= "SELECT * FROM cosmeticForm where cosmetic_number ='"+searchMess+"'";
+    }
+    else if(radioMess.equals("cosmetic_name")){
+      condition= "SELECT * FROM cosmeticForm where cosmetic_name LTKE'%"+searchMess+"%'";
+    }
+    else if(radioMess.equals("cosmetic_price")){
+      double max=0, min=0;
+      String regex="[^0123456789.]";
+      String []priceMess= searchMess.split(regex);
+      if(priceMess.length==1){
+        max= min= Double.parseDouble(priceMess[0]);
+      }
+      else if(priceMess.length== 2){
+        min= Double.parseDouble(priceMess[0]);
+        max= Double.parseDouble(priceMess[1]);
+        if(max<min){
+          double t = max;
+          max=min;
+          min=t;
+        }
+      }
+      else{
+        fail(request,response,"输入的价格格式有错误");
+        return;
+      }
+      condition= "SELECT * FROM cosmeticForm where"+"cosmetic_price <= "+max+" AND cosmetic_price >="+ min;
+    }
+    Httpsession session=request.getSession(true);
+    Connection con= null;
+    DataByPage dataBean= null;
+    try{
+      dataBean= (DataByPage)session.getAttribute("dataBean");
+      if(dataBean== null){
+        dataBean=new DataByPage();    //创建 Javabean对象
+        session.setAttribute("dataBean",dataBean);
+      }
+    }
+    catch(Exception exp){
+      dataBean=new DataByPage();    //创建 Javabean对象
+      session.setAttribute("dataBean",dataBean);
+    }
+    String url="jdbc:mysql://127.0.0.1/shop?"+"user=root&password=&characterEncoding=gb2312&serverTimezone=UTC";
+    try{
+      con= DriverManager.getConnection(url);
+      Statement sql= con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,Resultset.TYPE_CONCUR_READ_ONLY);
+      ResultSet rs= sql.executeQuery(condition);
+      rowSet= new CachedRowSetImpl();   //创建行集对象
+      rowSet.populate(rs);
+      dataBean.setRowSet(rowSet);   //行集数据存储在 dataBean中
+      con.close();    //关闭连接
+    }
+    catch(SQLException exp){}
+    response.sendRedirect("byPageShow.jsp");
+  }
+  public void doGet(HttpServletRequest request,HttpServeltResponse response) throws ServletException,IOException{
+    doPost(request,response);
+  }
+  public void fail(HttpServletRequest request,HttpServletResponse response,String backNews){
+    response.setContentType("text/html;charset= gb2312");
+    try{
+      PrintWriter out= response.getWriter();
+      out.println("<html><body>");
+      out.println("<h2>+backNews+</h2>");
+      out.println("返回:");
+      out.println("<a href=searchCosmetic.jsp>查询化妆品</a>");
+      out.println("</body></html>");
+    }
+    catch(IOException exp){}
+  }
+}
